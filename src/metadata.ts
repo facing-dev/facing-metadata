42efa3414933e8389d7239170b1cf3ffe802a1fa
li lei <ruojianll@gmail.com>
2023-10-29 07:46:14 +0800

    v1.0.1

--- src/metadata.ts
+++ src/metadata.ts
@@ -6,7 +6,7 @@ export class Metadata<T extends {
     constructor(symbol = Symbol('faple-metadata')) {
         this.symbol = symbol
     }
-    #travel(target: any, traveler: { (target: any, data: Partial<T>): boolean }) {
+    #travel(target: any, traveler: { (target: any, data: T): boolean }) {
         let t = target
         do {
             const data = this.getOwn(t)
@@ -19,10 +19,10 @@ export class Metadata<T extends {
             t = Object.getPrototypeOf(t)
         } while (t !== null)
     }
-    createData(): Partial<T> {
+    createData(): T {
         return Object.create(null)
     }
-    create(target: any, data?: Partial<T>) {
+    create(target: any, data?: T) {
         if (Object.getOwnPropertyDescriptor(target, this.symbol)) {
             error('Target had metadata')
         }
@@ -34,22 +34,22 @@ export class Metadata<T extends {
         })
     }
 
-    getOwn(target: any): Partial<T> | undefined {
+    getOwn(target: any): T | undefined {
         const des = Object.getOwnPropertyDescriptor(target, this.symbol)
         if (!des) {
             return undefined
         }
         return des.value
     }
-    get(target: any): Partial<T> | undefined {
-        let data: Partial<T> | undefined = undefined
+    get(target: any): T | undefined {
+        let data: T | undefined = undefined
         this.#travel(target, (target, _data) => {
             data = _data
             return false
         })
         return data
     }
-    obtainOwn(target: any, data?: Partial<T>): Partial<T> {
+    obtainOwn(target: any, data?: T) {
         const _data = this.getOwn(target)
         if (_data !== undefined) {
             return _data
@@ -58,9 +58,9 @@ export class Metadata<T extends {
         this.create(target, __data)
         return __data
     }
-    obtain(target: any, data?: Partial<T>): Partial<T> {
+    obtain(target: any, data?: T) {
         const _data = this.get(target)
-        
+
         if (_data !== undefined) {
             return _data
         }
@@ -74,12 +74,12 @@ export class Metadata<T extends {
     }
     setValue<K extends keyof T>(target: any, key: K, value: T[K]) {
         const data = this.obtain(target)
-    
+
         data[key] = value
     }
     getValueOwn<K extends keyof T>(target: any, key: K) {
         const data = this.getOwn(target)
-        if(data===undefined){
+        if (data === undefined) {
             return undefined
         }
         return data[key]
